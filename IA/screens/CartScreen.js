39eb61292170170b4205f2c3852a3a8eb0c65f90
Bertha-Jones <87230787+Bertha-Jones@users.noreply.github.com>
2021-11-12 20:45:27 +0000

    Updated the Cart screen

---
+++ IA/screens/CartScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, FlatList } from "react-native";
+
+export default function CartScreen(){
+
+    return(
+        <View>
+            <Text> This is the cart screen! </Text>
+        </View>
+    );
+}
