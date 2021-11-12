d664059b8b3858537157df5f7da9e46ee1090ee5
Bertha-Jones <87230787+Bertha-Jones@users.noreply.github.com>
2021-11-12 20:46:14 +0000

    Added the product details screen :)

---
+++ IA/screens/DetailScreen.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import COLORS from '../consts/colors';
+
+
+export default function DetailScreen({navigation, route}){
+
+    const products = route.params;
+
+    return(
+        <SafeAreaView style={{flex: 1, backgroundColor: '#ffffff'}}>
+            <View style={{
+                paddingHorizontal: 20,
+                marginTop: 20,
+                flexDirection: 'row',
+                justifyContent: 'space-between'
+            }}> 
+                <Icon name="arrow-back" size={28} onPress={() => navigation.goBack()}/>
+                <Icon name="shopping-cart" size={28} onPress={() => navigation.navigate('Cart')}/>
+            </View>
+
+            <View style={{
+                marginTop: 10,
+                flex: 0.45,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <Image source={{uri: products.image}} style={{
+                    height: 250,
+                    width: 220,
+                    resizeMode: 'contain',
+                    flex: 1
+                }}/>
+            </View>
+
+            <View style={{
+                flex: 0.55,
+                marginTop: 20,
+                backgroundColor: COLORS.light,
+                marginHorizontal: 7,
+                marginBottom: 7,
+                borderRadius: 20,
+            }}> 
+                <Text style={{marginTop: 10, textAlign: 'center', fontSize: 19, fontWeight: 'bold'}}>
+                    <Text> {products.title} </Text>
+                    <Text> {products.rating.rate} </Text>
+                </Text>
+               
+            </View>
+        </SafeAreaView>
+    );
+}
