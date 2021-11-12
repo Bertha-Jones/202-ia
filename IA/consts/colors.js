9a13c1e90d0c6646d2c690bf5c968e895ed3892b
Bertha-Jones <87230787+Bertha-Jones@users.noreply.github.com>
2021-11-12 20:45:06 +0000

    Added colors and product info...

---
+++ IA/consts/colors.js
@@ -0,0 +1,11 @@
+const COLORS = {
+    white: '#fff',
+    dark: '#000',
+    red: '#F52A2A',
+    light: '#F1F1F1',
+    green: '#00B761',
+    orange: '#ab3000',
+    lightbrown :'#e8ca92'
+  };
+  
+  export default COLORS;
