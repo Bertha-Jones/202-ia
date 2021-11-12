d99678fa2be7f143a7f812f2b47911f12b231f1b
Bertha-Jones <87230787+Bertha-Jones@users.noreply.github.com>
2021-11-12 20:44:02 +0000

    Changes on the main screen

--- IA/App.js
+++ IA/App.js
@@ -1,21 +1,43 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import SignUp from './screens/SignUp';
+import ProductScreen  from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import DetailScreen from './screens/DetailScreen';
+import MainScreen from './screens/MainScreen';
 
+const Main = createNativeStackNavigator();
 
-const Stack = createNativeStackNavigator();
-
-const MyStack = () => {
+export default function App () {
   return (
+
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Main.Navigator >
+        <Main.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: 'Welcome' }}
         />
-        <Stack.Screen {...HomeScreen} />
-      </Stack.Navigator>
+        <Main.Screen 
+         name="Main"
+         component={MainScreen}
+        />
+        <Main.Screen 
+          name="SignUp"
+          component={SignUp}
+        />
+        <Main.Screen
+          name="Cart"
+          component={CartScreen}
+        />
+        <Main.Screen
+          name='Details'
+          component={DetailScreen}
+          screenOptions={{
+        headerShown: true
+      }}/>
+      </Main.Navigator>
     </NavigationContainer>
   );
 };
